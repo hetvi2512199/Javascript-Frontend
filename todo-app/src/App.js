ef9872a14de6248f4b824228654cfbb21063f329
hetvi vyas <hetvi.vyas@digiqt.com>
2022-11-01 12:45:53 +0530

    local storage value store

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,15 +1,38 @@
+import { useState } from 'react';
 import './App.css';
-import ToDoList from './Components/ToDoList';
-
-
 
 
 function App() {
+
+  const [language,setLanguage] = useState('');
+  const [framework, setFramework] = useState('');
+
+  function saveChanges(){
+
+    var courses = JSON.parse(localStorage.getItem('courses') || "[]")
+    var course = {
+      language:language,
+      framework:framework
+    }
+    courses.push(course)
+    
+    localStorage.setItem('courses',JSON.stringify(courses))
+
+  }
+
+
   return (
-    <div className="App">
-      <ToDoList />
+    <div className="center">
+        <br/>
+            <h1>ToDoList</h1>
+        <br/>
+        <input type='text' placeholder="Language" value={language} onChange={(e)=>{setLanguage(e.target.value)}}>
+        </input><br/>
+        <input type='text' placeholder="Framework" value={framework} onChange={(e)=>{setFramework(e.target.value)}}>
+        </input><br/><br/>
+        <button className="newBtn"  onClick={saveChanges}>SAVE CHANGES</button>
 
-
+       
       
     </div>
   );
