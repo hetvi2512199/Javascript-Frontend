06ee4fc8f21703c82b8fe4e1de8137078a74c188
hetvi vyas <hetvi.vyas@digiqt.com>
2022-11-01 17:02:35 +0530

    task 3 complete with sorting

--- create-product/src/App.js
+++ create-product/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+    const [data,setData] = useState([])
+
+    useEffect(()=>{
+      fetch("https://dummyjson.com/products")
+      .then((result)=>{result.json()
+      .then((resp)=>{
+        const sortResult = resp.products.sort((a,b) => (a.price - b.price))
+        setData(sortResult)
+      })
+    })
+    },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> GET API</h2>
+      <table border="1">
+        <tr>
+          <td>ID</td>
+          <td>Thumbnail</td>
+          <td>Title</td>
+          <td>Description</td>
+          <td>Price</td>
+          <td>Rating</td>
+
+        </tr>
+        
+        {
+          data ? data.map((item)=>
+            
+            <tr>
+              <td>{item.id}</td>
+              <img src="{item[0].thumbnail}" alt="image"></img>
+              <td>{item.title}</td>
+              <td>{item.description}</td>
+              <td>{item.price}</td>
+              <td>{item.rating}</td>
+
+            </tr>
+          
+          ):"no data"
+        }
+      </table>
     </div>
   );
 }
